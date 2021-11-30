e65e9895c650091bec671880f4cd0b0918d2223e
steppenwolf0 <alejandro.lopezrn@hotmail.com>
2021-11-30 09:42:10 +0100

    Add files via upload

---
+++ b.R
@@ -0,0 +1,50 @@
+ThresholdingAlgo <- function(y,lag,threshold,influence) {
+  signals <- rep(0,length(y))
+  filteredY <- y[0:lag]
+  avgFilter <- NULL
+  stdFilter <- NULL
+  avgFilter[lag] <- mean(y[0:lag], na.rm=TRUE)
+  stdFilter[lag] <- sd(y[0:lag], na.rm=TRUE)
+  for (i in (lag+1):length(y)){
+    if (abs(y[i]-avgFilter[i-1]) > threshold*stdFilter[i-1]) {
+      if (y[i] > avgFilter[i-1]) {
+        signals[i] <- 1;
+      } else {
+        signals[i] <- -1;
+      }
+      filteredY[i] <- influence*y[i]+(1-influence)*filteredY[i-1]
+    } else {
+      signals[i] <- 0
+      filteredY[i] <- y[i]
+    }
+    avgFilter[i] <- mean(filteredY[(i-lag):i], na.rm=TRUE)
+    stdFilter[i] <- sd(filteredY[(i-lag):i], na.rm=TRUE)
+  }
+  return(list("signals"=signals,"avgFilter"=avgFilter,"stdFilter"=stdFilter))
+}
+
+# Data
+y <- read.table("b.csv", header= FALSE)
+#y <- c(y)
+y <- as.vector(unlist(y))
+
+lag       <- 30
+threshold <- 2.25
+influence <- 1
+# Run algo with lag = 30, threshold = 5, influence = 0
+result <- ThresholdingAlgo(y,lag,threshold,influence)
+
+# Plot result
+par(mfrow = c(2,1),oma = c(2,2,0,0) + 0.1,mar = c(0,0,2,1) + 0.2)
+plot(1:length(y),y,type="l",ylab="",xlab="") 
+lines(1:length(y),result$avgFilter,type="l",col="cyan",lwd=2)
+lines(1:length(y),result$avgFilter+threshold*result$stdFilter,type="l",col="green",lwd=2)
+lines(1:length(y),result$avgFilter-threshold*result$stdFilter,type="l",col="green",lwd=2)
+
+for (i in (lag+1):length(y)){
+	if(result$signals[i]!=0)
+	{
+		points(i,y[i],col="red",pch="+")	
+	}
+}
+plot(result$signals,type="S",col="red",ylab="",xlab="",ylim=c(-1.5,1.5),lwd=2)
